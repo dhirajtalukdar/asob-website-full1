9061d2f5ec8219b3c53033722094f122d16d4d6e
dhirajtalukdar <dhiraj06@gmail.com>
2025-07-29 20:58:35 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import WhatWeDo from './components/WhatWeDo';
-import Events from './components/Events';
-import Gallery from './components/Gallery';
-import GetInvolved from './components/GetInvolved';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Modals from './components/Modals';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import WhatWeDo from '@/components/WhatWeDo';
+import Events from '@/components/Events';
+import Gallery from '@/components/Gallery';
+import GetInvolved from '@/components/GetInvolved';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
+import Modals from '@/components/Modals';
 
 export default function Home() {
   return (
